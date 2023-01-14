56cdbc064774230b3fcab98f84b7095666a28990
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-14 10:47:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 TgFileDownloadlist = {}
 
